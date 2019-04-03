4143f9678edcebd8cb8fa6801d58bdf11347aeec
jainit1 <47260650+jainit1@users.noreply.github.com>
2019-04-02 22:07:24 -0400

    Add files via upload

---
+++ TinDog-Start-master/index.js
@@ -0,0 +1,130 @@
+
+
+// console.log("HEY AJEL");
+
+// function jainit() {
+//     a = 'chutiyo'
+//     b = 'jainit';
+//     c = a + ' ' + b;
+//     console.log(c);
+// }
+
+// jainit();
+// jainit();
+
+
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(3,5));
+// console.log(add(13,45));
+
+
+
+// function temp(celcius){
+//     console.log((fahrenheit = (9/5) * celcius + 32) + ' Degrees');  
+// }
+
+// temp(2);
+// temp(21);
+// temp(0);
+
+
+
+// function add(a,b){
+//     console.log((a+b));
+// }
+
+// add(51,7);
+// add(1,7);
+
+
+
+// number = [1,2,4,6];
+
+// for(var i in number){
+//     console.log(number[i]);
+// }
+
+
+// if grade = 90 = A;
+// if grade = 80 = B;
+// if grade <80 = F;
+
+
+// function grade(grade){
+//     if(grade > 90){
+//         console.log('A');
+//     } else if (grade >= 80){
+//         console.log('B');
+//     } else {
+//         console.log('F');
+//     }
+// }
+
+// grade(67);
+// grade(80);
+// grade(92);
+
+
+// 60+ speed = '50$ fine'
+// 70+ speed = '100$ fine'
+// 60< speed = NO FINE! YOU ARE SAFE!
+
+
+// Holiday = speed + 5... 
+
+// function speed(speed){
+//     if (speed >= 70){
+//         console.log('$100 Fine');
+//     } else if (speed >= 60 ){
+//         console.log('$50 Fine');
+//     } else {
+//         console.log('NO FINE! YOU ARE SAFE!')     
+//     } 
+// }
+
+// speed(70);
+
+
+function getMilk(money){
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+
+    var numberOfBottles = Math.floor(money / 1.5);
+
+    console.log("buy " + numberOfBottles + " bottles of milk");
+
+
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+getMilk(5);
+
+
+
+
+
+
+
+
+// for(let i = 1; i <= 10; i+=2){
+    
+//     console.log(i);
+// }
